7877dad56b2f9fdacb0e42ecd44039807b93a33f
JoãoCarlos <joaocarlos.losfe@gmail.com>
2021-07-02 11:22:36 -0300

    farmacia salva os dados no bd e recupera

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from base_de_dados import Database
 
+from farmacia import Farmacia
+
 database = Database()
 db_2 = Database()
 
 print(db_2 is database)
 
 print(db_2.total_instancias) # vai ser so uma pq ja foi instanciada antes
+
+farmacia = Farmacia()
+
+farmacia.abrir_farmacia()
